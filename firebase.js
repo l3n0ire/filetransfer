cc493a91632caf54b2f807f3ef1f85c7b0313ac5
HZ757 <HZ757@users.noreply.github.com>
2021-01-09 17:25:30 -0500

    Added code generator

--- firebase.js
+++ firebase.js
@@ -1,33 +1,59 @@
- // Your web app's Firebase configuration
- var firebaseConfig = {
-    apiKey: "AIzaSyAJI6C1VHAB7NwM-VjLx1NcIQ5-CfHREr8",
-    authDomain: "fir-storagetest-98e1a.firebaseapp.com",
-    projectId: "fir-storagetest-98e1a",
-    storageBucket: "fir-storagetest-98e1a.appspot.com",
-    messagingSenderId: "789913035588",
-    appId: "1:789913035588:web:57adb28fc34f698bf32d11"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  console.log(firebase);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAJI6C1VHAB7NwM-VjLx1NcIQ5-CfHREr8",
+  authDomain: "fir-storagetest-98e1a.firebaseapp.com",
+  projectId: "fir-storagetest-98e1a",
+  storageBucket: "fir-storagetest-98e1a.appspot.com",
+  messagingSenderId: "789913035588",
+  appId: "1:789913035588:web:57adb28fc34f698bf32d11"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase);
 
-    async function uploadImage(){
-        const ref = await firebase.storage().ref();
+async function random()
+{
+  let data = {
+    "jsonrpc": "2.0",
+    "method": "generateStrings",
+    "params": {
+        "apiKey": "53b56391-d01d-40b7-984b-1a92afe63182",
+        "n": 1,
+        "length": 6,
+        "characters": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "replacement": true
+    },
+    "id": 42
+  }
+  const response = await fetch('https://api.random.org/json-rpc/2/invoke', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+  })
 
-        const file = document.querySelector("#photo").files[0];
-        const name = new Date() + '-' + file.name;
-        const metadata ={
-            contentType:file.type
-        }
+  const code = await response.json()
 
-        const imageRef = await ref.child(name);
-        const snapshot = await imageRef.put(file,metadata);
-        console.log(snapshot)
-        const url = await snapshot.ref.getDownloadURL();
-        
-        console.log(url)
-        alert("image Upload Successful")
-        const linkElement = document.querySelector('#link');
-        linkElement.href = url;
+  document.querySelector("#code").innerHTML = code.result.random.data[0]
+}
+
+async function uploadImage(){
+    
+    const ref = await firebase.storage().ref();
+
+    const file = document.querySelector("#photo").files[0];
+    const name = new Date() + '-' + file.name;
+    const metadata ={
+        contentType:file.type
+    }
+
+    const imageRef = await ref.child(name);
+    const snapshot = await imageRef.put(file,metadata);
+    console.log(snapshot)
+    const url = await snapshot.ref.getDownloadURL();
+    
+    console.log(url)
+    alert("image Upload Successful")
+    const linkElement = document.querySelector('#link');
+    linkElement.href = url;
       
 }
