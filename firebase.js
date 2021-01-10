f54784e86c1f553724dd31e61518fc3482585b83
Colin Lin <colincool100@gmail.com>
2021-01-09 19:20:16 -0500

    code to database on file upload

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ var firebaseConfig = {
   apiKey: "AIzaSyAJI6C1VHAB7NwM-VjLx1NcIQ5-CfHREr8",
   authDomain: "fir-storagetest-98e1a.firebaseapp.com",
   projectId: "fir-storagetest-98e1a",
+  databaseURL: "https://fir-storagetest-98e1a-default-rtdb.firebaseio.com/",
   storageBucket: "fir-storagetest-98e1a.appspot.com",
   messagingSenderId: "789913035588",
   appId: "1:789913035588:web:57adb28fc34f698bf32d11"
@@ -36,9 +37,19 @@ async function random()
   return randomCode
 }
 
+async function addToDB(randomCode,link,password,expireTime){
+
+  const res = await firebase.database().ref("users/"+randomCode).set({
+    randomCode:randomCode,
+    link:link,
+    password:password,
+    expireTime:expireTime
+  });
+
+}
 async function uploadImage(){
-    let randomCode = await random()
-    document.querySelector("#code").innerHTML = randomCode
+    let randomCode = await random();
+    document.querySelector("#code").innerHTML = randomCode;
     
     const ref = await firebase.storage().ref();
 
@@ -50,12 +61,15 @@ async function uploadImage(){
 
     const imageRef = await ref.child(randomCode + "/" + name);
     const snapshot = await imageRef.put(file,metadata);
-    console.log(snapshot)
+    console.log(snapshot);
     const url = await snapshot.ref.getDownloadURL();
     
-    console.log(url)
-    alert("image Upload Successful")
+    console.log(url);
+    alert("image Upload Successful");
     const linkElement = document.querySelector('#link');
     linkElement.href = url;
+
+    // store in db
+    await addToDB(randomCode,url,"","");
       
 }
