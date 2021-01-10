ebc7d16103a69bfb1926ec5971409e88c7b30544
Colin Lin <colincool100@gmail.com>
2021-01-10 15:10:26 -0500

    upload file and api keys

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config();
 const app = express();
+const firebase = require('firebase');
 
-//load env variables
-dotenv.config({path: './config/config.env'});
+
 // body parser
 app.use(express.json());
 // set static folder
 app.use(express.static(path.join(__dirname,'public')))
+// firebase config
+var firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    databaseURL: process.env.databaseURL,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  };
+var firebaseApp = firebase.initializeApp(firebaseConfig);
 
 // download file
 app.get('/api/files/:downloadCode', function (req, res) {
@@ -16,10 +27,34 @@ app.get('/api/files/:downloadCode', function (req, res) {
     res.send(req.params.downloadCode)
 })
 
+// get keys
+app.get('/api/keys', function (req, res) {
+    res.send(firebaseConfig)
+})
+
 // upload file
 app.post('/api/files', function (req, res) {
-    res.send('POST request to the homepage')
+    if("downloadCode" in req.body && "password" in req.body && "expireTime" in req.body){
+        try{
+            addToDB(req.body.downloadCode,req.body.password,req.body.expireTime);
+            res.send({"message":"added to db"})
+        }catch(error){
+            console.log(error)
+            res.status(500).json("server error")
+        }
+    }
+    else
+        res.status(400).json("missing parameter")
 })
+
+async function addToDB(downloadCode,password,expireTime){
+
+    const res = await firebaseApp.database().ref("users/"+downloadCode).set({
+      downloadCode:downloadCode,
+      password:password,
+      expireTime:expireTime
+    });  
+}
 
 
 const PORT= process.env.PORT || 5000;
